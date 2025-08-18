838ff7b7531244375645c092189a99a0cdcb30b8
dima <dmitriikomarow@gmail.com>
2025-08-18 16:17:51 +0300

    fix

--- src/components/ProjectSliders.tsx
+++ src/components/ProjectSliders.tsx
@@ -27,13 +27,13 @@ function ProjectSlider({ project }: ProjectSliderProps) {
         id={project.href.replace('#', '')}
         className="snap-section"
       >
-        <div className="flex justify-end w-full ">
+        <div className="flex justify-end w-full mb-10">
           {imageUrls.length > 0 ? (
             <Slider
               images={imageUrls}
               alt={project.label}
               width={isSmallHeight ? "min(100vw, 665px)" : "min(100vw, 100vh)"}
-              height={isSmallHeight ? "min(97vh, 665px)" : "min(100vw, 97vh)"}
+              height={isSmallHeight ? "min(97.5vh, 665px)" : "min(100vw, 97.5vh)"}
               className={`w-screen h-full`}
             />
           ) : (
@@ -42,7 +42,7 @@ function ProjectSlider({ project }: ProjectSliderProps) {
             </div>
           )}
         </div>
-                 <div className={`text-left mt-10 flex justify-end`}>
+        <div className={`text-left flex justify-end`}>
            <div className={`${isSmallHeight ? 'w-[665px]' : 'w-[min(100vw,100vh)]'}`}>
             <h2 className="transition-all duration-200 [word-spacing:0.3em] tracking-widest font-[800] text-[14px] uppercase cursor-pointer text-[#141414]">{project.title}</h2>
             <p className="transition-all duration-200 [word-spacing:0.3em] tracking-widest font-[400] text-[14px] uppercase cursor-pointer text-[#141414] mt-4 indent-12">
@@ -59,7 +59,7 @@ function ProjectSlider({ project }: ProjectSliderProps) {
 
 export default function ProjectSliders() {
   return (
-    <div className="space-y-2 snap-section">
+    <div className="snap-section">
       {projectsData.map((project) => (
         <ProjectSlider key={project.id} project={project} />
       ))}
