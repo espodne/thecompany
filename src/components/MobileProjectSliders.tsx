129dd283b9b4f264ad3935c761c5e5b7bc626186
dima <dmitriikomarow@gmail.com>
2025-09-04 18:07:01 +0300

    fix: mobile snap

--- src/components/MobileProjectSliders.tsx
+++ src/components/MobileProjectSliders.tsx
@@ -20,7 +20,7 @@ function MobileProjectSlider({ project }: ProjectSliderProps) {
   return (
     <section
       id={project.href.replace('#', '')}
-      className="snap-start flex flex-col justify-start pt-[140px]"
+      className="snap-start flex flex-col justify-start pt-[60px]"
     >
       <div className="flex justify-center mb-4">
         {imageUrls.length > 0 ? (
